8867783c198bcf4a5af3dbd7602ab8a6f8f59302
Aditya Chavan <chavanadityawork@gmail.com>
2024-01-06 15:38:59 +0000

    Changes Made to README and SQL file, added Image with Column names, description and Data types

--- README.txt
+++ README.txt
@@ -8,16 +8,32 @@ ABOUT:
 3)Finally the study aims to know how different strategies can be optimized during Holiday and Non-Holiday weeks at different stores for diffeent prodcuts to optimize the markdowns and increase sales.
 
 DATASET DESCRIPTION:
-The Dataset was obtained from Kaggle Walmart Sales Forecasting Competition.
+The Dataset was obtained from https://github.com/Princekrampah/WalmartSalesAnalysis.git
 The Dataset contains transactions from 3 differnet branches namely, Yangon, Naypyitaw and Mandalay
-The Dataset contains 17 columns and 1001 rows.
+The Dataset contains 17 columns and 1000 rows.
 
 COLUMN DESCRIPTION:
+Check Image Table for description of columns.
 
 DATA ANALYSIS:
 
 APPROACH USED:
 
 QUESTIONS TO ANSWER:
+SET 1:
+1) How many unique product lines does the data have?
+2) What product category is the best seller defined by largest revenue?
+3) What is the most common product line by gender?
+
+SET 2:
+1)What was the most common method of payment across different product lines?
+2)Which Customer type brings the most revenue?
+3)Does being a Member/Non-Member affect the rating given to the Purchase?
+
+SET 3:
+1) What is the ratio of Member/Non-Member for each branch?
+2) In what time frame(months) was the COGS highest?
+3) Which method of payment is more oftenly used across the branches? 
+
 
 FORMULAS AND CALCULATIONS:
--- Walmart-Sales-Data-Analysis.sql
+++ Walmart-Sales-Data-Analysis.sql
@@ -1,27 +1,48 @@
 CREATE DATABASE IF NOT EXISTS WalmartSalesData;
 
+-- Initialize Database
 USE WalmartSalesData;
 
+-- Create Sales Table with Necessary Columns 
 CREATE TABLE IF NOT EXISTS Sales(
-	Invoice_ID VARCHAR (15) NOT NULL UNIQUE PRIMARY KEY,
-    Branch VARCHAR (2) NOT NULL,
-    City VARCHAR(10) NOT NULL,
-    Customer VARCHAR(15) NOT NULL,
+	Invoice_ID VARCHAR (30) NOT NULL UNIQUE PRIMARY KEY,
+    Branch VARCHAR (5) NOT NULL,
+    City VARCHAR(30) NOT NULL,
+    Customer VARCHAR(30) NOT NULL,
     Gender VARCHAR(10) NOT NULL,
-    Product_line VARCHAR(25) NOT NULL,
-    Unit_price DECIMAL(5, 2) NOT NULL,
+    Product_line VARCHAR(100) NOT NULL,
+    Unit_price DECIMAL(10, 2) NOT NULL,
     Quantity INT NOT NULL,
     Tax FLOAT NOT NULL,
-    Total_value DECIMAL (7, 5) NOT NULL,
+    Total_value DECIMAL (10, 5) NOT NULL,
 	Purchase_date DATE NOT NULL,
     Purchase_time TIME NOT NULL,
     Payment_type VARCHAR(15) NOT NULL,
-    COGS DECIMAL(5,2) NOT NULL,
+    COGS DECIMAL(10,2) NOT NULL,
     Gross_margin_percent FLOAT NOT NULL,
-    Gross_income DECIMAL (5,5) NOT NULL,
+    Gross_income DECIMAL (10,2) NOT NULL,
     Rating FLOAT NOT NULL
 );
 
-
+DROP TABLE sales;
 SELECT *
 FROM Sales;
+
+-- How many unique product lines does the data have?
+
+-- What product category is the best seller defined by largest  revenue?
+
+-- What is the most common product line by gender?
+
+-- What was the most common method of payment across different product lines?
+
+-- Which customer type brings the most revenue?
+
+-- Does being a Member/Non-Member affect the rating given to the Purchase?
+
+-- What is the ratio of Member/Non-Member for each branch?
+
+-- In what time frame(months) was the COGS highest?
+
+-- Which method of payment is more oftenly used across the branches?
+
